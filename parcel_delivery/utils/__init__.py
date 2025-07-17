3de1b5b16cabb0596986f3fc38c7ed27f812957e
Allan Barongo <122985155+A-Barongo@users.noreply.github.com>
2025-07-17 21:39:33 +0300

    Delete parcel_delivery directory

--- parcel_delivery/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .google_maps import GoogleMapsService
-from .email_service import EmailService
-from .cost_calculator import CostCalculator
-
-# Only export classes, don't initialize here
-__all__ = ['GoogleMapsService', 'EmailService', 'CostCalculator']
--- parcel_delivery/utils/cost_calculator.py
+++
@@ -1,6 +0,0 @@
-class CostCalculator:
-    def calculate(self, weight, distance):
-        base = 5.0
-        weight_cost = weight * 2.5
-        distance_cost = (distance / 1000) * 0.15
-        return round(base + weight_cost + distance_cost, 2)
