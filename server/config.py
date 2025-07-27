d0490fcce880f6b9bcc8e10b5390606e9c296fe3
Allan <allanbarongo5@gmail.com>
2025-07-27 22:21:14 +0300

    Added functionality for admin route to get parcels by their id

--- server/config.py
+++ server/config.py
@@ -79,7 +79,7 @@ def create_app(test_config=None):
     from server.routes.auth_routes import Login
     from server.routes.admin_routes import (
         AdminParcelList, UpdateParcelStatus, UpdateParcelLocation,
-        ParcelHistoryList, ParcelHistoryDetail
+        ParcelHistoryList, ParcelHistoryDetail,AdminParcelDetail
     )
     from server.routes.parcels import ParcelList, ParcelResource, ParcelCancel, ParcelDestination, ParcelStatus
     api.add_resource(Home, '/')
@@ -88,6 +88,7 @@ def create_app(test_config=None):
     api.add_resource(Logout, '/logout')
     api.add_resource(Profile, '/profile')
     api.add_resource(AdminParcelList, '/admin/parcels')
+    api.add_resource(AdminParcelDetail, '/admin/parcels/<int:parcel_id>')
     api.add_resource(UpdateParcelStatus, '/admin/parcels/<int:id>/status')
     api.add_resource(UpdateParcelLocation, '/admin/parcels/<int:id>/location')
     api.add_resource(ParcelHistoryList, '/admin/histories')
